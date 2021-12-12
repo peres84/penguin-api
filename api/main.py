27611098ecdc69c25654d6452046c0776444186b
Javier Peres <peresrjavier@gmail.com>
2021-12-12 16:58:40 -0400

    new api
    working with heroku

--- api/main.py
+++ api/main.py
@@ -4,5 +4,10 @@ from src.controllers.distribution_details import app
 from src.controllers.penguin_breeding import app
 from src.controllers.welcome_test import app
 from src.controllers.add import app
+from dotenv import load_dotenv 
+import os
 
-app.run(debug=True)
+load_dotenv()
+port = os.getenv("PORT")
+app.run('0.0.0.0',port, debug=True)
+
